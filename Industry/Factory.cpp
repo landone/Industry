1152a889fe68b26743899e8acc0c15cfbb853cce
Landon Ellis <landon.ellis2@gmail.com>
2020-05-05 23:49:24 -0400

    Modeling
    Boiler & Pipe

--- Industry/Factory.cpp
+++ Industry/Factory.cpp
@@ -5,7 +5,6 @@ Factory::Factory() {
 	mdl.setModel("models/DefaultFactory.obj");
 	mdl.setPos(glm::vec3(4, 0, -2));
 
-	test.setModel("models/Boiler.obj");
-	test.setPos(glm::vec3(4, 0, -2));
+	test.setModel("models/Pipe.obj");
 
 }
